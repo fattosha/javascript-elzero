5ed655a6c9e6daa7529d61a7b732ccfd3f5b3b73
menna khalid <mnhk32134@gmail.com>
2026-08-27 19:06:17 +0300

    r

---
+++ tasks 23-26/task1/assets/js/plugin.js
@@ -0,0 +1,10 @@
+console.log(100000); // 100000
+console.log(100 _000); // 100000
+console.log(1e5); // 100000
+console.log(10e4); // 100000
+console.log(100e3); // 100000
+console.log(1 _000e2); // 100000
+console.log(10 _000e1); // 100000
+console.log(100 _000e0); // 100000
+console.log(0.1e6); // 100000
+console.log(100000.0); // 100000
---
+++ tasks 23-26/task7/assets/js/plugin.js
@@ -0,0 +1,1 @@
+console.log(Math.floor(Math.random() * 10 % 5)); // 0  1  2  3  4
