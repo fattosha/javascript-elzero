1a4be18c6b34ed6d08ff3d8cd5f8547a13f46194
menna Khalid <fattoshak@gmail.com>
2026-08-28 15:58:49 +0300

    Create Plugin.js

---
+++ Tasks 33-37/Task5/Plugin.js
@@ -0,0 +1,75 @@
+
+
+<pre>
+<span>let num1 = 10;</span>
+<span>let num2 = 9;</span>
+<span>let num3 = 10;</span>
+<span>let num4 = 32;</span>
+
+/*
+  Do Not Edit Below This Line
+  Needed Output
+  True 7 Times
+*/
+
+// Condition 1
+
+if (num1 &gt; num2) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 2
+
+if (num1 &gt; num2 &amp;&amp; num1 &lt; num4) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 3
+
+if (num1 &gt; num2 &amp;&amp; num1 === num3) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 4
+
+if ((num1 + num2) &lt; num4) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 5
+
+if ((num1 + num3) &lt; num4) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 6
+
+if ((num1 + num2 + num3) &lt; num4) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 7
+
+if (num4 - (num1 + num3) + num2 === 21) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+</pre>
+
+num1 = 10
+num2 = 9
+num3 = 10
+num4 = 32
